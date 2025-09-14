f4528982075c8b8ef1ab5a31b21e83570fbd4f7f
Subodh Kumar  Soni <103482618+Dheeraj678@users.noreply.github.com>
2025-09-14 23:47:43 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class FirstProgram{
+  public static void main(String args[])
+  {
+   System.out.print("Welcome");
+   System.out.print("To");
+   System.out.print("My");
+   System.out.print("Coding Practice Repository");
+}
+}
