65ca2a64900a9295d78b058ef9c493e93f1ce4c8
Subodh Kumar  Soni <103482618+Dheeraj678@users.noreply.github.com>
2025-09-14 23:55:16 +0530

    With New Line

---
+++ FirstProgram1.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class FirstProgram1{
+  public static void main(String args[])
+  {
+   System.out.println("Welcome");
+   System.out.println("To");
+   System.out.println("My");
+   System.out.println("Coding Practice Repository");
+}
+}
